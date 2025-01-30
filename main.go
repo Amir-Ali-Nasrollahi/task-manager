75aa7fd658518fc863819e25fac0d3fe6f9e2283
Amir-Ali-Nasrollahi <nasrollahiamirali@gmail.com>
2025-01-30 14:44:31 +0330

    make update function

--- main.go
+++ main.go
@@ -4,25 +4,46 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
-func readPrompt() string {
+func readPrompt() []string {
 
 	reader := bufio.NewReader(os.Stdin)
 	text, _ := reader.ReadString('\n')
 
 	text = strings.ReplaceAll(text, "\n", "")
+	explode := strings.Split(text, " ")
 
-	return text
+	return explode
 }
 
 func main() {
 
 	task := Task{}
+	var ID int = 0
 
-	value := readPrompt()
-	fmt.Println(value)
-	task.Add(2, "makeGoLang", "in-progcessing")
-	task.ShowAll()
+	for {
+		/*
+		*
+		* Get Prompt case and check with switch
+		*
+		**/
+
+		fmt.Println("enter your entry : ")
+		res := readPrompt()
+
+		switch res[0] {
+		case "add":
+			ID++
+			fmt.Println(res)
+			task.Add(ID, res[1])
+		case "list":
+			task.ShowAll()
+		case "update":
+			id, _ := strconv.Atoi(res[1])
+			task.Update(id, res[2])
+		}
+	}
 }
--- task.go
+++ task.go
@@ -9,9 +9,9 @@ type Todo struct {
 }
 type Task struct {
 	/*
-	
+
 	* it means make slice which each item be a Todo struct
-	* 
+	*
 	* if you say []int -> means make slice that every item is int
 
 	**/
@@ -19,10 +19,26 @@ type Task struct {
 	NextID int
 }
 
-func (t *Task) Add(ID int, Name string, Status string) {
+func (t Task) CheckTodoExist(id int) (int, int) {
+	flag := 0
+	var todo_key int
+	for key, value := range t.todo {
+
+		if value.ID <= id && value.ID == id {
+			todo_key = key
+			flag = 1
+			break
+		}
+
+	}
+
+	return todo_key, flag
+}
+
+func (t *Task) Add(ID int, Name string) {
 	nextID := ID + 1
 
-	t.todo = append(t.todo, Todo{ID, Name, Status})
+	t.todo = append(t.todo, Todo{ID, Name, "todo"})
 
 	t.NextID = nextID
 
@@ -31,5 +47,27 @@ func (t *Task) Add(ID int, Name string, Status string) {
 func (t Task) ShowAll() {
 
 	fmt.Println(t.todo)
+
+}
+
+func (t *Task) Update(id int, newName string) {
+
+	flag := 0
+	var todo_key int
+	for key, value := range t.todo {
+
+		if value.ID <= id && value.ID == id {
+			todo_key = key
+			flag = 1
+			break
+		}
+
+	}
+	if flag == 1 {
+		t.todo[todo_key] = Todo{id, newName, "todo"}
+	}
+}
+
+func (t *Task) Delete(id int) {
 
 }
